6438bd88a9a7edac561749954d1b92880605f833
Angular CLI <angular-cli@angular.io>
2017-07-02 22:53:22 +0100

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { NikodemOneNgPage } from './app.po';
+
+describe('nikodem-one-ng App', () => {
+  let page: NikodemOneNgPage;
+
+  beforeEach(() => {
+    page = new NikodemOneNgPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
