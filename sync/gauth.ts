5f6e41d5a7ed9108804df806e9b0b86153773a79
Pawel Piotrowski <pawel@flytype.org>
2017-07-03 01:30:55 +0100

    sync google auth init fs-extra added

---
+++ sync/gauth.ts
@@ -0,0 +1,9 @@
+import * as fs from 'fs-extra';
+
+export class GoogleAuth {
+    private clientSecretFile = '';
+
+    getClientSecret(): void {
+        console.log('GoogleAuth says hello');
+    }
+}
