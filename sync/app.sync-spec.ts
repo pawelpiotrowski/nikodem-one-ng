05791a06ef232a26e69655ddfa83461dd57bffd9
Pawel Piotrowski <pawel@flytype.org>
2017-07-03 01:14:13 +0100

    added mocha, chia and sync app unit test

---
+++ sync/app.sync-spec.ts
@@ -0,0 +1,8 @@
+import { AppSync } from './app.sync';
+import { expect } from 'chai';
+
+describe('AppSync', () => {
+    it('should create the app with property "init"', () => {
+        expect(new AppSync()).to.have.property('init');
+    });
+});
